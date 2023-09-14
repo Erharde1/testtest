bfdb00382695512faeb0c09b5490e31bdca74157
Erharde1 <143745983+Erharde1@users.noreply.github.com>
2023-09-14 10:45:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-#boucle while
 
 import random
-
-def jeux_devinette():
-    nombre=random.randit(start,end)
-    essai= none
+def jeu_devinette():
+    print("J'ai choisi un nombre au hasard entre 0 et 1000. À vous de le deviner...")
+    nombre=random.randint(0,1000)
+    essai = -1
     while essai != nombre:
-        essai=int(input(f"J'ai choisi un nombre au hasard entre 0 et 1000. À vous de le deviner... \nEntrez votre essai: "))
+        essai = int(input("Entrez votre essai: "))
         if essai < nombre:
             print("Mauvais choix, le nombre est plus grand.")
         elif essai > nombre:
             print("Maucais choix, le nombre est plus petit.")
-    print("Bravo! Vous avez deviné le chiffre.")
+    print("Bravo!")
 
-jeux_devinette(0,1000)
+jeu_devinette()
